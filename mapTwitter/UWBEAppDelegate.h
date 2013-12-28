58b4845b4c07f8325a8591ecf0218a41aac51ef3
jindulys <857367901@qq.com>
2013-12-28 17:39:05 -0500

    Inititial Commit

---
+++ mapTwitter/UWBEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UWBEAppDelegate.h
+//  mapTwitter
+//
+//  Created by Li Yansong on 13-12-28.
+//  Copyright (c) 2013年 Liyansong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UWBEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
