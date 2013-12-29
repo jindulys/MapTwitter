d759c30e7ddda81608d98d42e58adbea17b2c748
jindulys <857367901@qq.com>
2013-12-29 16:29:24 -0500

    Finished the navigationBar

---
+++ mapTwitter/MTStyles.h
@@ -0,0 +1,16 @@
+//
+//  MTStyles.h
+//  mapTwitter
+//
+//  Created by Li Yansong on 13-12-28.
+//  Copyright (c) 2013年 Liyansong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTStyles : NSObject
+
+
+// Colors
++ (UIColor *)newKidOnTheBlockBlue;
+@end
---
+++ mapTwitter/SearchHistoryVC.h
@@ -0,0 +1,13 @@
+//
+//  SearchHistoryVC.h
+//  mapTwitter
+//
+//  Created by Li Yansong on 13-12-28.
+//  Copyright (c) 2013年 Liyansong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchHistoryVC : UIViewController
+
+@end
