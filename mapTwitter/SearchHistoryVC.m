7819d94192bbfdc81ec62010193113ae4d925c72
jindulys <857367901@qq.com>
2013-12-29 19:41:22 -0500

    Finished GoogleMAPVC -V0.1

--- mapTwitter/SearchHistoryVC.m
+++ mapTwitter/SearchHistoryVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "SearchHistoryVC.h"
+#import "searchMapVC.h"
 
 @interface SearchHistoryVC ()
 
@@ -28,12 +29,32 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.title = @"Search History";
+    
+    // Map Button
+    UIBarButtonItem *button = nil;
+    UIButton *topicsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [topicsButton setImage:[UIImage imageNamed:@"icon-reader-topics"] forState:UIControlStateNormal];
+    [topicsButton setImage:[UIImage imageNamed:@"icon-reader-topics-active"] forState:UIControlStateHighlighted];
+    
+    CGSize imageSize = [UIImage imageNamed:@"icon-reader-topics"].size;
+    topicsButton.frame = CGRectMake(0.0, 0.0, imageSize.width, imageSize.height);
+    topicsButton.contentEdgeInsets = UIEdgeInsetsMake(0, 16, 0, -16);
+    [topicsButton addTarget:self action:@selector(mapAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    button = [[UIBarButtonItem alloc] initWithCustomView:topicsButton];
+    self.navigationItem.rightBarButtonItem = button;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)mapAction:(id)sender
+{
+    searchMapVC *searchMap = [[searchMapVC alloc] init];
+    [self.navigationController pushViewController:searchMap animated:YES];
 }
 
 @end
--- mapTwitter/searchMapVC.m
+++ mapTwitter/searchMapVC.m
@@ -7,8 +7,18 @@
 //
 
 #import "searchMapVC.h"
+#import <GoogleMaps/GoogleMaps.h>
 
-@interface searchMapVC ()
+@interface searchMapVC () <GMSMapViewDelegate>
+{
+    GMSMapView *_mapView;
+    GMSGeocoder *_geoCoder;
+}
+
+@property (nonatomic, strong) GMSMarker *searchMarker;
+@property (nonatomic) CLLocationCoordinate2D searchLocation;
+
+@property (nonatomic) BOOL isFinishedShowCurrentLocation;
 
 @end
 
@@ -27,6 +37,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithTitle:@"Search" style:UIBarButtonItemStylePlain target:self action:@selector(searchAction:)];
+    self.navigationItem.rightBarButtonItem = button;
+    
+    // Create a GMSCameraPosition that tells the map to display the
+    // Coordinate at current location
+    GMSCameraPosition *camera = [GMSCameraPosition
+                                 cameraWithLatitude:-33.86
+                                          longitude:151.20
+                                               zoom:6];
+    _mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    _mapView.myLocationEnabled = YES;
+    _mapView.delegate = self;
+    self.view = _mapView;
+    
+    _geoCoder = [[GMSGeocoder alloc] init];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [_mapView addObserver:self forKeyPath:@"myLocation" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [_mapView removeObserver:self forKeyPath:@"myLocation"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +72,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"myLocation"]&&[object isKindOfClass:[GMSMapView class]]) {
+        if (!self.isFinishedShowCurrentLocation) {
+            [_mapView animateToCameraPosition:[GMSCameraPosition cameraWithLatitude:_mapView.myLocation.coordinate.latitude longitude:_mapView.myLocation.coordinate.longitude zoom:16]];
+            self.isFinishedShowCurrentLocation = YES;
+        }
+    }
+}
+
+- (void)searchAction:(id)sender
+{
+    NSLog(@"Search btn pressed");
+}
+
+#pragma mark - GMSMapViewDelegate
+- (void)mapView:(GMSMapView *)mapView didLongPressAtCoordinate:(CLLocationCoordinate2D)coordinate
+{
+    __weak GMSMapView *wmapView = _mapView;
+    GMSReverseGeocodeCallback handler = ^(GMSReverseGeocodeResponse *response, NSError *error) {
+        GMSMapView *smapView = wmapView;
+        if (response && response.firstResult) {
+            GMSMarker *marker = [[GMSMarker alloc] init];
+            marker.position = coordinate;
+            marker.title = response.firstResult.addressLine1;
+            marker.snippet = response.firstResult.addressLine2;
+            marker.appearAnimation = kGMSMarkerAnimationPop;
+            [smapView clear];
+            marker.map = smapView;
+        }else{
+            NSLog(@"Could not reverse geocode point (%f,%f): %@",coordinate.latitude,coordinate.longitude, error);
+        }
+    };
+    self.searchLocation = coordinate;
+    [_geoCoder reverseGeocodeCoordinate:coordinate completionHandler:handler];
+}
 @end
